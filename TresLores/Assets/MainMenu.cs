336233cf4f8c49d2e351cda8f78dd147c66c7d2f
mankindmember12 <130433831+mankindmember12@users.noreply.github.com>
2023-04-11 22:12:02 +0700

    Penambahan Loading

--- TresLores/Assets/MainMenu.cs
+++ TresLores/Assets/MainMenu.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public void PlayGame(){
+        SceneManager.LoadScene("Loading");
+    }
+
     public void QuitGame(){
         Application.Quit();
         UnityEditor.EditorApplication.isPlaying = false;
