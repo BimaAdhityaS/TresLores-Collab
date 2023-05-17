2d25e44db5d675ebb162140516305a9a588a5f65
mankindmember12 <130433831+mankindmember12@users.noreply.github.com>
2023-05-18 00:19:30 +0700

    Adding Party Member Mechanic

---
+++ TresLores/Assets/Script/AI & Stuff/PartyMembers.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PartyMembers : MonoBehaviour
+{
+    public GameObject target;
+    public List<Vector3> positions;
+    public int distance_permitted;
+    public float speed;
+    private Vector3 lastLeaderPosition;
+    // Use this for initialization
+    void Start () {
+        positions.Add(target.transform.position);
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        if (lastLeaderPosition != positions[positions.Count - 1])
+        {
+            positions.Add(target.transform.position);
+        }        
+    
+        if (positions.Count >= distance_permitted)
+        {
+            if (gameObject.transform.position != positions[0])
+            {
+                
+                transform.position = Vector3.MoveTowards(transform.position, positions[0], Time.deltaTime * speed);
+        
+            }
+            else
+            {
+                positions.Remove(positions[0]);
+                gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+            }
+        }
+        lastLeaderPosition = target.transform.position;
+    }
+}
