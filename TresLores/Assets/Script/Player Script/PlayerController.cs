fcfc0794148b971427117a64795e513e668c9cc3
mankindmember12 <130433831+mankindmember12@users.noreply.github.com>
2023-05-17 23:59:37 +0700

    Level, character animation, top down

---
+++ TresLores/Assets/Script/Player Script/PlayerController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D myRB;
+     public float speed = 5f;
+    
+    public Animator animator;
+
+     Vector2 movement;
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");        
+
+        animator.SetFloat("Horizontal",movement.x);
+        animator.SetFloat("Vertical",movement.y);
+        animator.SetFloat("Speed",movement.sqrMagnitude);
+
+    }
+
+    void FixedUpdate(){
+        myRB.MovePosition(myRB.position + movement*speed*Time.fixedDeltaTime);
+    }
+
+}
